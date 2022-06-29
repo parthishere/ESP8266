cdd7badca56a5d0105f4e63f99f5d86d3428a695
parthishere <parthishere1234@gmail.com>
2022-06-30 01:13:51 +0530

    chn\angr

--- index/api/serializers.py
+++ index/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from index.models import Esp8266
+from index.models import Esp8266, Applience, ChangeTime
 
 from hashid_field import HashidField
 
@@ -8,8 +8,19 @@ class EspSerializer(serializers.ModelSerializer):
     # mac = HashidField(null=True, blank=True)
     class Meta:
         model = Esp8266
-        exclude = ['mac']
+        fields = "__all__"
         
     # def create(self, validated_data):
     #     Esp8266.objects.create(pot=validated_data['pot'])
     #     pass
+    
+    
+class AppliencesSerializer(serializers.ModelSerializers):
+    class Meta:
+        model = Esp8266
+        fields = "__all__"
+        
+class ChangeTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChangeTime
+        fields = "__all__"
